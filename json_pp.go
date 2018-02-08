afac84011deff6da78cb64bbc4f3f43a7adc6a6c
Adam Jacob Muller <adam@isprime.com>
2018-02-08 00:58:13 -0500

    add functions to reformat strings/byte strings

--- json_pp.go
+++ json_pp.go
@@ -13,3 +13,33 @@ func JSON_PP(object interface{}) error {
 	fmt.Printf("%s\n", string(bytes))
 	return nil
 }
+
+func JSON_PP_string(text string) error {
+	var object interface{}
+	err := json.Unmarshal([]byte(text), &object)
+	if err != nil {
+		return err
+	}
+
+	bytes, err := json.MarshalIndent(object, "", " ")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%s\n", string(bytes))
+	return nil
+}
+
+func JSON_PP_bytes(ubytes []byte) error {
+	var object interface{}
+	err := json.Unmarshal(ubytes, &object)
+	if err != nil {
+		return err
+	}
+
+	bytes, err := json.MarshalIndent(object, "", " ")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%s\n", string(bytes))
+	return nil
+}
